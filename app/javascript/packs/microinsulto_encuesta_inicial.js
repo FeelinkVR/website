ac5fbafb209700f81a701a01c5a872b7c9a7751d
Esteban Alonso Barrios Araya <esteban.barrios@sansano.usm.cl>
2022-11-08 18:42:00 -0300

    evitar paso en cuestionarios

--- app/javascript/packs/microinsulto_encuesta_inicial.js
+++ app/javascript/packs/microinsulto_encuesta_inicial.js
@@ -5,41 +5,82 @@ $(document).ready(function() {
     var left, opacity, scale; //fieldset properties which we will animate
     var animating; //flag to prevent quick multi-click glitches
 
-    $(".next-microinsulto-inicial").click(function() {
-        if (animating) return false;
-        animating = true;
+    var back = 0;
+    var next= true;
+    var pregunta1 = $("[name='microinsulto_encuesta_inicial[pregunta1]']");
+    var pregunta2 = $("[name='microinsulto_encuesta_inicial[pregunta2]']");
+    var pregunta3 = $("[name='microinsulto_encuesta_inicial[pregunta3]']");
+    var pregunta4 = $("[name='microinsulto_encuesta_inicial[pregunta4]']");
+    var pregunta5 = $("[name='microinsulto_encuesta_inicial[pregunta5]']");
+    var pregunta6 = $("[name='microinsulto_encuesta_inicial[pregunta6]']");
+    var pregunta7 = $("[name='microinsulto_encuesta_inicial[pregunta7]']");
 
-        current_fs = $(this).parent();
-        next_fs = $(this).parent().next();
 
-        //activate next step on progressbar using the index of next_fs
-        $("#progressbar-microinsulto-inicial li").eq($("fieldset").index(next_fs)).addClass("active");
-
-        //show the next fieldset
-        next_fs.show();
-        //hide the current fieldset with style
-        current_fs.animate({ opacity: 0 }, {
-            step: function(now, mx) {
-                //as the opacity of current_fs reduces to 0 - stored in "now"
-                //1. scale current_fs down to 80%
-                scale = 1 - (1 - now) * 0.2;
-                //2. bring next_fs from the right(50%)
-                left = (now * 50) + "%";
-                //3. increase opacity of next_fs to 1 as it moves in
-                opacity = 1 - now;
-                current_fs.css({ 'transform': 'scale(' + scale + ')' });
-                next_fs.css({ 'left': left, 'opacity': opacity });
-            },
-            duration: 500,
-            complete: function() {
-                current_fs.hide();
-                animating = false;
-            },
-            //this comes from the custom easing plugin
-            easing: 'easeOutQuint'
-        });
+    pregunta1.on("click",function(){
+        next = true;
+    });
+    pregunta2.on("click",function(){
+        next = true;
+    });
+    pregunta3.on("click",function(){
+        next = true;
+    });
+    pregunta4.on("click",function(){
+        next = true;
+    });
+    pregunta5.on("click",function(){
+        next = true;
+    });
+    pregunta6.on("click",function(){
+        next = true;
+    });
+    pregunta7.on("click",function(){
+        next = true;
     });
 
+    $(".next-microinsulto-inicial").click(function() {
+        if (next){
+            if (animating) return false;
+            animating = true;
+
+            current_fs = $(this).parent();
+            next_fs = $(this).parent().next();
+
+            //activate next step on progressbar using the index of next_fs
+            $("#progressbar-microinsulto-inicial li").eq($("fieldset").index(next_fs)).addClass("active");
+
+            //show the next fieldset
+            next_fs.show();
+            //hide the current fieldset with style
+            current_fs.animate({ opacity: 0 }, {
+                step: function(now, mx) {
+                    //as the opacity of current_fs reduces to 0 - stored in "now"
+                    //1. scale current_fs down to 80%
+                    scale = 1 - (1 - now) * 0.2;
+                    //2. bring next_fs from the right(50%)
+                    left = (now * 50) + "%";
+                    //3. increase opacity of next_fs to 1 as it moves in
+                    opacity = 1 - now;
+                    current_fs.css({ 'transform': 'scale(' + scale + ')' });
+                    next_fs.css({ 'left': left, 'opacity': opacity });
+                },
+                duration: 500,
+                complete: function() {
+                    current_fs.hide();
+                    animating = false;
+                },
+                //this comes from the custom easing plugin
+                easing: 'easeOutQuint'
+            });
+
+        }
+        if (back>0){
+            back -= 1;
+        } 
+        if (back == 0){
+            next = false;
+        }      
+    });
     $(".previous-microinsulto-incial").click(function() {
         if (animating) return false;
         animating = true;
@@ -73,5 +114,7 @@ $(document).ready(function() {
             //this comes from the custom easing plugin
             easing: 'easeOutQuint'
         });
+        next = true;
+        back +=1 ;
     });
 });
