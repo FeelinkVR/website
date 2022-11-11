c1364e303169ca4b0c2ed186b74751d33901b11e
Adrián Alonso Menares Jiménez <adrian.menares@alumnos.inf.utfsm.cl>
2022-11-11 20:42:10 -0300

    microinvalidacion encuesta final fix

--- app/controllers/microinvalidacion_encuesta_finals_controller.rb
+++ app/controllers/microinvalidacion_encuesta_finals_controller.rb
@@ -1,4 +1,10 @@
 class MicroinvalidacionEncuestaFinalsController < ApplicationController
+
+    def show
+        @encuesta = MicroinvalidacionEncuestaFinal.find(params[:id])
+        @user = User.find(params[:user_id])
+    end
+
     def new
         @user = User.find(params[:user_id])
         if !@user.microinvalidacion_encuesta_final.nil?
@@ -19,7 +25,7 @@ class MicroinvalidacionEncuestaFinalsController < ApplicationController
 
         if @encuesta.save
             flash[:notice] = "Encuesta completada con exito"
-            redirect_to microinvalidacion_path(@user.id)
+            redirect_to user_microinvalidacion_encuesta_final_path(user_id: @user, id: @encuesta.id)
         else
             flash[:alert] = @encuesta.errors.full_messages.join(', ')
         end
