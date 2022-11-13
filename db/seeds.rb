5b5b98d91a102389f0b7199de694a8cff9e6deba
Adrián Alonso Menares Jiménez <adrian.menares@alumnos.inf.utfsm.cl>
2022-11-13 16:31:10 -0300

    añadir otra empresa

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,7 +40,24 @@ Simulation.create(user_id: 5, tipo: "Microinsulto", perspectiva: "Victimario", r
 Simulation.create(user_id: 6, tipo: "Microinsulto", perspectiva: "Victimario", realizada: true, decisiones: '{"cantidad": 3,"conducta": [0,0,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
 Simulation.create(user_id: 7, tipo: "Microinsulto", perspectiva: "Victimario", realizada: true, decisiones: '{"cantidad": 3,"conducta": [1,-1,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
 
+#MICROASALTO
+Simulation.create(user_id: 4, tipo: "Microasalto", perspectiva: "Testigo", realizada: true, decisiones:'{"cantidad": 3,"conducta": [-1,0,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+Simulation.create(user_id: 5, tipo: "Microasalto", perspectiva: "Testigo", realizada: true, decisiones:'{"cantidad": 3,"conducta": [-1,0,0],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+Simulation.create(user_id: 6, tipo: "Microasalto", perspectiva: "Testigo", realizada: true, decisiones:'{"cantidad": 3,"conducta": [-1,0,-1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+Simulation.create(user_id: 7, tipo: "Microasalto", perspectiva: "Testigo", realizada: true, decisiones:'{"cantidad": 3,"conducta": [1,0,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+
+Simulation.create(user_id: 4, tipo: "Microasalto", perspectiva: "Victima", realizada: true, decisiones: '{"cantidad": 3,"conducta": [1,0,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+Simulation.create(user_id: 5, tipo: "Microasalto", perspectiva: "Victima", realizada: true, decisiones: '{"cantidad": 3,"conducta": [0,0,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+Simulation.create(user_id: 6, tipo: "Microasalto", perspectiva: "Victima", realizada: true, decisiones: '{"cantidad": 3,"conducta": [1,0,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+Simulation.create(user_id: 7, tipo: "Microasalto", perspectiva: "Victima", realizada: true, decisiones: '{"cantidad": 3,"conducta": [-1,-1,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+
+Simulation.create(user_id: 4, tipo: "Microasalto", perspectiva: "Victimario", realizada: true, decisiones: '{"cantidad": 3,"conducta": [1,0,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+Simulation.create(user_id: 5, tipo: "Microasalto", perspectiva: "Victimario", realizada: true, decisiones: '{"cantidad": 3,"conducta": [0,0,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+Simulation.create(user_id: 6, tipo: "Microasalto", perspectiva: "Victimario", realizada: true, decisiones: '{"cantidad": 3,"conducta": [0,0,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+Simulation.create(user_id: 7, tipo: "Microasalto", perspectiva: "Victimario", realizada: true, decisiones: '{"cantidad": 3,"conducta": [1,-1,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+
 #MICROINVALIDACION
+Simulation.create(user_id: 3, tipo: "Microinvalidacion", perspectiva: "Testigo", realizada: true, decisiones:'{"cantidad": 3,"conducta": [-1,0,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
 Simulation.create(user_id: 4, tipo: "Microinvalidacion", perspectiva: "Testigo", realizada: true, decisiones:'{"cantidad": 3,"conducta": [-1,0,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
 Simulation.create(user_id: 5, tipo: "Microinvalidacion", perspectiva: "Testigo", realizada: true, decisiones:'{"cantidad": 3,"conducta": [-1,0,0],"texto": ["Ignorar","Tirar silla","Llorar"]}')
 Simulation.create(user_id: 6, tipo: "Microinvalidacion", perspectiva: "Testigo", realizada: true, decisiones:'{"cantidad": 3,"conducta": [-1,0,-1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
@@ -91,3 +108,105 @@ MicroinvalidacionEncuestaFinal.create(user_id: 5, pregunta1: true, pregunta2: tr
 MicroinvalidacionEncuestaFinal.create(user_id: 6, pregunta1: true, pregunta2: true,  pregunta3: true, pregunta4: false)
 MicroinvalidacionEncuestaFinal.create(user_id: 7, pregunta1: true, pregunta2: false, pregunta3: true, pregunta4: nil)
 
+
+#--------------------------------------------------------------------------EMPRESA 2------------------------------------------------------------------------------
+Empresa.create(nombre: "Riot Games", direccion: "Grieta del Invocador", oficina: "1", comuna: "San Joaquín", region: "Metropolitana", rut: "12345678-1")
+
+#Cuenta Admin;
+#Cuenta Encargado:
+User.create(email: 'encargado@riotgames.cl', password: '123456', password_confirmation: '123456', telefono: '123456789', nombre: 'Riot', apellido: 'Games', admin: false, encargado: true, empresa_id: 2)
+#Cuentas Empleado:
+User.create(email: 'empleado@riotgames.cl', password: '123456', password_confirmation: '123456', telefono: '123456789', nombre: 'Teemo', apellido: 'Trol', admin: false, encargado: false, empresa_id: 2)
+User.create(email: 'garen@riotgames.cl', password: '123456', password_confirmation: '123456', telefono: '123456789', nombre: 'Garen', apellido: 'Demacia', admin: false, encargado: false, empresa_id: 2)
+User.create(email: 'akali@riotgames.cl', password: '123456', password_confirmation: '123456', telefono: '123456789', nombre: 'Akali', apellido: 'KDA', admin: false, encargado: false, empresa_id: 2)
+User.create(email: 'darius@riotgames.cl', password: '123456', password_confirmation: '123456', telefono: '123456789', nombre: 'Darius', apellido: 'Noxus', admin: false, encargado: false, empresa_id: 2)
+User.create(email: 'jhin@riotgames.cl', password: '123456', password_confirmation: '123456', telefono: '123456789', nombre: 'Jhin', apellido: 'Khada', admin: false, encargado: false, empresa_id: 2)
+
+#------------Simulaciones-----------
+
+#MICROINSULTO
+Simulation.create(user_id: 8, tipo: "Microinsulto", perspectiva: "Testigo", realizada: true, decisiones:'{"cantidad": 3,"conducta": [-1,0,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+Simulation.create(user_id: 9, tipo: "Microinsulto", perspectiva: "Testigo", realizada: true, decisiones:'{"cantidad": 3,"conducta": [-1,0,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+Simulation.create(user_id: 10, tipo: "Microinsulto", perspectiva: "Testigo", realizada: true, decisiones:'{"cantidad": 3,"conducta": [-1,0,0],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+Simulation.create(user_id: 11, tipo: "Microinsulto", perspectiva: "Testigo", realizada: true, decisiones:'{"cantidad": 3,"conducta": [-1,0,-1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+Simulation.create(user_id: 12, tipo: "Microinsulto", perspectiva: "Testigo", realizada: true, decisiones:'{"cantidad": 3,"conducta": [1,0,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+
+Simulation.create(user_id: 8, tipo: "Microinsulto", perspectiva: "Victima", realizada: true, decisiones: '{"cantidad": 3,"conducta": [1,0,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+Simulation.create(user_id: 9, tipo: "Microinsulto", perspectiva: "Victima", realizada: true, decisiones: '{"cantidad": 3,"conducta": [0,0,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+Simulation.create(user_id: 10, tipo: "Microinsulto", perspectiva: "Victima", realizada: true, decisiones: '{"cantidad": 3,"conducta": [1,0,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+Simulation.create(user_id: 11, tipo: "Microinsulto", perspectiva: "Victima", realizada: true, decisiones: '{"cantidad": 3,"conducta": [-1,-1,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+Simulation.create(user_id: 12, tipo: "Microinsulto", perspectiva: "Victima", realizada: true, decisiones: '{"cantidad": 3,"conducta": [-1,-1,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+
+Simulation.create(user_id: 8, tipo: "Microinsulto", perspectiva: "Victimario", realizada: true, decisiones: '{"cantidad": 3,"conducta": [1,0,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+Simulation.create(user_id: 9, tipo: "Microinsulto", perspectiva: "Victimario", realizada: true, decisiones: '{"cantidad": 3,"conducta": [0,0,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+Simulation.create(user_id: 10, tipo: "Microinsulto", perspectiva: "Victimario", realizada: true, decisiones: '{"cantidad": 3,"conducta": [0,0,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+Simulation.create(user_id: 11, tipo: "Microinsulto", perspectiva: "Victimario", realizada: true, decisiones: '{"cantidad": 3,"conducta": [1,-1,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+Simulation.create(user_id: 12, tipo: "Microinsulto", perspectiva: "Victima", realizada: true, decisiones: '{"cantidad": 3,"conducta": [-1,-1,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+
+#MICROASALTO
+Simulation.create(user_id: 8, tipo: "Microasalto", perspectiva: "Testigo", realizada: true, decisiones:'{"cantidad": 3,"conducta": [-1,0,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+Simulation.create(user_id: 9, tipo: "Microasalto", perspectiva: "Testigo", realizada: true, decisiones:'{"cantidad": 3,"conducta": [-1,0,0],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+Simulation.create(user_id: 10, tipo: "Microasalto", perspectiva: "Testigo", realizada: true, decisiones:'{"cantidad": 3,"conducta": [-1,0,-1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+Simulation.create(user_id: 11, tipo: "Microasalto", perspectiva: "Testigo", realizada: true, decisiones:'{"cantidad": 3,"conducta": [1,0,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+
+Simulation.create(user_id: 8, tipo: "Microasalto", perspectiva: "Victima", realizada: true, decisiones: '{"cantidad": 3,"conducta": [1,0,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+Simulation.create(user_id: 9, tipo: "Microasalto", perspectiva: "Victima", realizada: true, decisiones: '{"cantidad": 3,"conducta": [0,0,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+Simulation.create(user_id: 10, tipo: "Microasalto", perspectiva: "Victima", realizada: true, decisiones: '{"cantidad": 3,"conducta": [1,0,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+Simulation.create(user_id: 11, tipo: "Microasalto", perspectiva: "Victima", realizada: true, decisiones: '{"cantidad": 3,"conducta": [-1,-1,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+
+Simulation.create(user_id: 8, tipo: "Microasalto", perspectiva: "Victimario", realizada: true, decisiones: '{"cantidad": 3,"conducta": [1,0,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+Simulation.create(user_id: 9, tipo: "Microasalto", perspectiva: "Victimario", realizada: true, decisiones: '{"cantidad": 3,"conducta": [0,0,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+Simulation.create(user_id: 10, tipo: "Microasalto", perspectiva: "Victimario", realizada: true, decisiones: '{"cantidad": 3,"conducta": [0,0,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+Simulation.create(user_id: 11, tipo: "Microasalto", perspectiva: "Victimario", realizada: true, decisiones: '{"cantidad": 3,"conducta": [1,-1,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+
+#MICROINVALIDACION
+Simulation.create(user_id: 8, tipo: "Microinvalidacion", perspectiva: "Testigo", realizada: true, decisiones:'{"cantidad": 3,"conducta": [-1,0,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+Simulation.create(user_id: 9, tipo: "Microinvalidacion", perspectiva: "Testigo", realizada: true, decisiones:'{"cantidad": 3,"conducta": [-1,0,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+Simulation.create(user_id: 10, tipo: "Microinvalidacion", perspectiva: "Testigo", realizada: true, decisiones:'{"cantidad": 3,"conducta": [-1,0,0],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+Simulation.create(user_id: 11, tipo: "Microinvalidacion", perspectiva: "Testigo", realizada: true, decisiones:'{"cantidad": 3,"conducta": [-1,0,-1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+Simulation.create(user_id: 12, tipo: "Microinvalidacion", perspectiva: "Testigo", realizada: true, decisiones:'{"cantidad": 3,"conducta": [1,0,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+
+Simulation.create(user_id: 8, tipo: "Microinvalidacion", perspectiva: "Victima", realizada: true, decisiones: '{"cantidad": 3,"conducta": [1,0,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+Simulation.create(user_id: 9, tipo: "Microinvalidacion", perspectiva: "Victima", realizada: true, decisiones: '{"cantidad": 3,"conducta": [0,0,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+Simulation.create(user_id: 10, tipo: "Microinvalidacion", perspectiva: "Victima", realizada: true, decisiones: '{"cantidad": 3,"conducta": [1,0,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+Simulation.create(user_id: 11, tipo: "Microinvalidacion", perspectiva: "Victima", realizada: true, decisiones: '{"cantidad": 3,"conducta": [-1,-1,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+
+Simulation.create(user_id: 8, tipo: "Microinvalidacion", perspectiva: "Victimario", realizada: true, decisiones: '{"cantidad": 3,"conducta": [1,0,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+Simulation.create(user_id: 9, tipo: "Microinvalidacion", perspectiva: "Victimario", realizada: true, decisiones: '{"cantidad": 3,"conducta": [0,0,1],"texto": ["Ignorar","Tirar silla","Llorar"]}')
+Simulation.create(user_id: 10, tipo: "Microinvalidacion", perspectiva: "Victimario", realizada: false )
+Simulation.create(user_id: 11, tipo: "Microinvalidacion", perspectiva: "Victimario", realizada: false )
+
+#--------------------Encuestas----------------------------
+
+#MICROINSULTO
+MicroinsultoEncuestaInicial.create(user_id: 8, pregunta1: true,  pregunta2: true ,  pregunta3: true, pregunta4: false, pregunta5: false, pregunta6: false, pregunta7: true)
+MicroinsultoEncuestaInicial.create(user_id: 9, pregunta1: false, pregunta2: true ,  pregunta3: true, pregunta4: false, pregunta5: false, pregunta6: false, pregunta7: true)
+MicroinsultoEncuestaInicial.create(user_id: 10, pregunta1: false, pregunta2: true , pregunta3: true, pregunta4: true, pregunta5: true, pregunta6: true, pregunta7: true)
+MicroinsultoEncuestaInicial.create(user_id: 11, pregunta1: false, pregunta2: true , pregunta3: true, pregunta4: true, pregunta5: true, pregunta6: true, pregunta7: nil)
+
+MicroinsultoEncuestaFinal.create(user_id: 8, pregunta1: true, pregunta2: true, pregunta3: true,  pregunta4: true)
+MicroinsultoEncuestaFinal.create(user_id: 9, pregunta1: true, pregunta2: true, pregunta3: true,  pregunta4: true)
+MicroinsultoEncuestaFinal.create(user_id: 10, pregunta1: true, pregunta2: true, pregunta3: false, pregunta4: false)
+MicroinsultoEncuestaFinal.create(user_id: 11, pregunta1: true, pregunta2: true, pregunta3: false, pregunta4: nil)
+
+#MICROASALTO
+MicroasaltoEncuestaInicial.create(user_id: 8, pregunta1: false,  pregunta2: true ,  pregunta3: false, pregunta4: false, pregunta5: false, pregunta6: false, pregunta7: true)
+MicroasaltoEncuestaInicial.create(user_id: 9, pregunta1: false, pregunta2: true ,  pregunta3: true, pregunta4: true, pregunta5: true, pregunta6: false, pregunta7: true)
+MicroasaltoEncuestaInicial.create(user_id: 10, pregunta1: false, pregunta2: true , pregunta3: false, pregunta4: false, pregunta5: false, pregunta6: false, pregunta7: false)
+MicroasaltoEncuestaInicial.create(user_id: 11, pregunta1: true, pregunta2: true , pregunta3: true, pregunta4: true, pregunta5: true, pregunta6: true, pregunta7: nil)
+
+MicroasaltoEncuestaFinal.create(user_id: 8, pregunta1: true, pregunta2: true, pregunta3: false,  pregunta4: true)
+MicroasaltoEncuestaFinal.create(user_id: 9, pregunta1: true, pregunta2: true, pregunta3: true,  pregunta4: true)
+MicroasaltoEncuestaFinal.create(user_id: 10, pregunta1: true, pregunta2: true, pregunta3: true, pregunta4: true)
+MicroasaltoEncuestaFinal.create(user_id: 11, pregunta1: true, pregunta2: true, pregunta3: true, pregunta4: nil)
+
+#MICROINVALIDACION
+MicroinvalidacionEncuestaInicial.create(user_id: 8, pregunta1: true,  pregunta2: true,  pregunta3: true, pregunta4: true,  pregunta5: true,  pregunta6: false, pregunta7: true)
+MicroinvalidacionEncuestaInicial.create(user_id: 9, pregunta1: true, pregunta2: true,  pregunta3: true, pregunta4: false, pregunta5: false, pregunta6: true,  pregunta7: true)
+MicroinvalidacionEncuestaInicial.create(user_id: 10, pregunta1: true, pregunta2: true, pregunta3: true, pregunta4: true,  pregunta5: false, pregunta6: true, pregunta7: true)
+MicroinvalidacionEncuestaInicial.create(user_id: 11, pregunta1: true, pregunta2: true,  pregunta3: true, pregunta4: true, pregunta5: true, pregunta6: false, pregunta7: nil)
+
+MicroinvalidacionEncuestaFinal.create(user_id: 8, pregunta1: true, pregunta2: true,  pregunta3: true, pregunta4: true)
+MicroinvalidacionEncuestaFinal.create(user_id: 9, pregunta1: true, pregunta2: true,  pregunta3: true, pregunta4: false)
+MicroinvalidacionEncuestaFinal.create(user_id: 10, pregunta1: true, pregunta2: true,  pregunta3: true, pregunta4: false)
+MicroinvalidacionEncuestaFinal.create(user_id: 11, pregunta1: true, pregunta2: true, pregunta3: true, pregunta4: nil)
